f9fb50f69f6cc5dbda80614da99e9ecf2e58d443
anshu-govtech <anshu@dsaid.gov.sg>
2023-08-02 20:59:34 +0800

    (major) Refactored code for SDV models -- PAR, CTGAN, TVAE & GaussianCopula

--- commons/utils.py
+++ commons/utils.py
@@ -1,27 +1,103 @@
 "Utility functions"
-
+from sdv.datasets.demo import download_demo
 from sdv.metadata import SingleTableMetadata
 
-# TODO: add more info about the data
+# def get_execution_scores_obj():
+#     return {
+#         "Synthesizer": [],
+#         "Dataset": [],
+#         # "Dataset_Size_MB": [],
+#         "Train_Time": [],
+#         "Peak_Memory_MB": [],
+#         "Synthesizer_Size_MB": [],
+#         "Sample_Time": [],
+#         "Device": []
+#         # "Evaluate_Time": [],
+#     }
 
 
-def get_execution_scores_obj():
-    return {
-        "Synthesizer": [],
-        "Dataset": [],
-        # "Dataset_Size_MB": [],
-        "Train_Time": [],
-        "Peak_Memory_MB": [],
-        "Synthesizer_Size_MB": [],
-        "Sample_Time": [],
-        "Device": []
-        # "Evaluate_Time": [],
-    }
-
-
-def detect_metadata(real_data_df):
+def detect_metadata_with_sdv(real_data_df):
+    """
+    """
     metadata = SingleTableMetadata()
     metadata.detect_from_dataframe(data=real_data_df)
     # pprint(metadata.to_dict())
 #     python_dict = metadata.to_dict()
     return metadata
+
+
+def get_dataset_with_sdv(modality, dataset_name):
+    """
+    ------------------------
+    single-table: 
+    ------------------------
+    >> from sdv.datasets.demo import get_available_demos 
+    >> get_available_demos(modality='single_table')
+                        dataset_name  size_MB num_tables
+        0                   KRK_v1     0.07          1
+        1                    adult     3.91          1
+        2                    alarm     4.52          1
+        3                     asia     1.28          1
+        4                   census    98.17          1
+        5          census_extended     4.95          1
+        6                    child     3.20          1
+        7                  covtype   255.65          1
+        8                   credit    68.35          1
+        9       expedia_hotel_logs     0.20          1
+        10          fake_companies     0.00          1
+        11       fake_hotel_guests     0.03          1
+        12                    grid     0.32          1
+        13                   gridr     0.32          1
+        14               insurance     3.34          1
+        15               intrusion   162.04          1
+        16                 mnist12    81.20          1
+        17                 mnist28   439.60          1
+        18                    news    18.71          1
+        19                    ring     0.32          1
+        20      student_placements     0.03          1
+        21  student_placements_pii     0.03          1
+
+    ------------------------
+    sequential:
+    ------------------------
+    >> from sdv.datasets.demo import get_available_demos 
+    >> get_available_demos(modality='sequential')
+                                dataset_name  size_MB num_tables
+            0   ArticularyWordRecognition     8.61          1
+            1          AtrialFibrillation     0.92          1
+            2                BasicMotions     0.64          1
+            3       CharacterTrajectories    19.19          1
+            4                     Cricket    17.24          1
+            5               DuckDuckGeese   291.38          1
+            6                       ERing     1.25          1
+            7                  EchoNASDAQ     4.16          1
+            8                  EigenWorms   372.63          1
+            9                    Epilepsy     3.17          1
+            10       EthanolConcentration    51.38          1
+            11              FaceDetection   691.06          1
+            12            FingerMovements     5.32          1
+            13      HandMovementDirection    10.48          1
+            14                Handwriting     8.51          1
+            15                  Heartbeat    86.14          1
+            16             InsectWingbeat   547.59          1
+            17             JapaneseVowels     1.28          1
+            18                       LSST    14.18          1
+            19                     Libras     0.78          1
+            20               MotorImagery   616.90          1
+            21                     NATOPS     4.11          1
+            22                    PEMS-SF   490.15          1
+            23                  PenDigits     4.22          1
+            24             PhonemeSpectra   173.63          1
+            25               RacketSports     0.73          1
+            26         SelfRegulationSCP1    40.21          1
+            27         SelfRegulationSCP2    38.52          1
+            28         SpokenArabicDigits    47.63          1
+            29              StandWalkJump     4.32          1
+            30        UWaveGestureLibrary     7.76          1
+            31             nasdaq100_2019     1.65          1
+    """
+    real_data, metadata = download_demo(
+        modality=modality,
+        dataset_name=dataset_name,
+    )
+    return real_data, metadata
---
+++ run_sdv_model.py
@@ -0,0 +1,273 @@
+import logging
+import os
+import json 
+import pickle
+import time
+import tracemalloc
+import warnings
+
+import sdv
+from commons.static_vals import (N_BYTES_IN_MB, DataModalities)
+
+# SDV synthesizers 
+from synthesizers.tabular.sdv.copulas_synthesizer import \
+    GaussianCopulaSynthesizer
+from synthesizers.tabular.sdv.gen_synthesizer import (CTGANSynthesizer,
+                                                      TVAESynthesizer)
+from synthesizers.sequential.sdv.par_synthesizer import PARSynthesizer
+
+# from sdv.evaluation.single_table import evaluate_quality, run_diagnostic
+# from sdv.single_table import TVAESynthesizer
+# from sdv.single_table import CTGANSynthesizer
+# from sdv.single_table import GaussianCopulaSynthesizer
+# from sdv.sequential import PARSynthesizer
+
+# Links to Transform and Anonymize
+# https://docs.sdv.dev/sdv/single-table-data/modeling/synthetic-data-workflow/transform-and-anonymize
+
+with warnings.catch_warnings():
+    warnings.simplefilter("ignore")
+
+
+LOGGER = logging.getLogger(__name__)
+LOGGER.setLevel(logging.DEBUG)
+
+# tabular = DataModalities.TABULAR.value
+
+SYNTHESIZERS_MAPPING = {
+    # The CTGAN Synthesizer uses GAN-based DL methods
+    # deep learning methods to train a model and generate synthetic data.
+    "ctgan": CTGANSynthesizer,
+    # The TVAE Synthesizer uses a variational autoencoder (VAE)-based DL methods
+    "tvae": TVAESynthesizer,
+    # The Gaussian Copula Synthesizer uses classic,stattical methods
+    "gaussian_copula": GaussianCopulaSynthesizer,
+    # The PARSynthesizer uses a DL methods
+    "par": PARSynthesizer
+}
+
+# synthesizers = EXP_SYNTHESIZERS[tabular]
+# datasets = EXP_DATASETS[tabular]
+
+# TODO:
+# storing metadata -- load from the dict
+
+def run_model(**kwargs):
+    """"""
+    data_modality=kwargs["exp_data_modality"]
+    synthesizer_name=kwargs["exp_synthesizer"]
+    dataset_name=kwargs["exp_data"]
+    use_gpu=kwargs["use_gpu"]
+    num_epochs=kwargs["num_epochs"]
+    # data_folder=kwargs["data_folder"]
+    output_path=kwargs["output_path"]
+    real_dataset=kwargs["real_dataset"]
+    metadata=kwargs["metadata"]
+    # num_samples=kwargs["num_samples"]
+
+
+    if "sequential_details" in kwargs:
+        num_sequences = kwargs["sequential_details"]["num_sequences"] 
+        max_sequence_length = kwargs["sequential_details"]["max_sequence_length"] 
+        seq_fixed_attributes = kwargs["sequential_details"]["fixed_attributes"] 
+
+    synthesizer_class = SYNTHESIZERS_MAPPING[synthesizer_name]
+    num_samples = len(real_dataset)
+
+    # TODO: Might need to save metadata JSON file for some datasets
+    # metadata = detect_metadata(real_data)
+    LOGGER.info(metadata)
+
+    tracemalloc.start()
+
+    if synthesizer_name == "gaussian_copula":
+        # ---------------------
+        # Gaussain Copula
+        # ---------------------
+        # Learns - (1) Marginal Distributions (2) Covariance
+        # Link to the paper: https://dai.lids.mit.edu/wp-content/uploads/2018/03/SDV.pdf
+        # Params:
+        # numerical_distributions = {
+        #     <column name>: 'norm',
+        # },
+        # One of: 'norm' 'beta', 'truncnorm', 'uniform', 'gamma' or 'gaussian_kde'
+        # default_distribution <str> ='beta'
+        synthesizer = synthesizer_class(metadata)
+    elif synthesizer_name == "par": # sequential
+        # ---------------------------------------------
+        # PAR is a Probabilistic Auto-Regressive model 
+        # ---------------------------------------------
+        # - Learns how to create brand new sequences of multi-dimensional data, 
+        #   by conditioning on the unchanging, context values.
+        # - Models non-numerical columns, including columns with missing values.
+        # - Designed to work on multi-sequence data
+        # - Link to the paper: https://arxiv.org/pdf/2207.14406.pdf
+        # Params:
+        # context_columns <list>: do not vary inside of a sequence.    
+        synthesizer = synthesizer_class(metadata,
+                                        context_columns=seq_fixed_attributes,
+                                        epochs=num_epochs,
+                                        cuda=use_gpu, 
+                                        verbose=True,)
+        
+    elif synthesizer_name == "ctgan":
+        # --------------------------
+        # Conditional Table GAN
+        # --------------------------
+        # Params:
+        # epochs: The optimal number of epochs depends on both the complexity of your dataset 
+        #         and the metrics you are using to quantify success.
+        # links: https://github.com/sdv-dev/SDV/discussions/980
+        #        https://datacebo.com/blog/interpreting-ctgan-progress/
+        synthesizer = synthesizer_class(metadata, 
+                                        epochs=num_epochs,
+                                        cuda=use_gpu,
+                                        verbose=True)
+    elif synthesizer_name == "tvae":
+        # --------------------------
+        # TVAE
+        # --------------------------
+        synthesizer = synthesizer_class(metadata,
+                                epochs=num_epochs,
+                                cuda=use_gpu)
+
+    LOGGER.info(synthesizer.get_parameters())
+
+    begin_time = time.time()
+    # ---------------------
+    # Train
+    # ---------------------
+    synthesizer.fit(real_dataset)
+
+    if synthesizer_name == "gaussian_copula":
+        LOGGER.info(synthesizer.get_learned_distributions())
+    train_time = time.time()
+    # ---------------------
+    # Sample
+    # ---------------------
+    if synthesizer_name == "par":
+        # Params: 
+        # num_sequences: An integer >0 describing the number of sequences to sample
+        # sequence_length:
+        #     An integer >0 describing the length of each sequence.
+        #     If you provide None, the synthesizer will determine the lengths algorithmically
+        #     , and the length may be different for each sequence. Defaults to None.
+        synthetic_data = synthesizer.sample(num_sequences=num_sequences,
+                                            sequence_length=max_sequence_length)
+    else:
+        synthetic_data = synthesizer.sample(num_rows=num_samples)
+    sampling_time = time.time()
+
+    peak_memory = tracemalloc.get_traced_memory()[1] / N_BYTES_IN_MB
+    tracemalloc.stop()
+    tracemalloc.clear_traces()
+
+    print("#"*10)
+    # TODO: replace with logs
+    print(
+        f"Model {synthesizer_name} trained on {dataset_name} and sampled.")
+    print("#"*10)
+    
+    # ---------------------
+    # Outputs
+    # ---------------------
+    synthesizer_size = len(pickle.dumps(synthesizer)) / N_BYTES_IN_MB
+
+    # Get the memory usage of the real and synthetic DataFrame in bytes
+    real_dataset_size = real_dataset.memory_usage(deep=True).sum() / N_BYTES_IN_MB
+    synthetic_dataset_size = synthetic_data.memory_usage(deep=True).sum() / N_BYTES_IN_MB
+
+    # execution_scores = {
+    #     # "Date": [today_date],
+    #     "Lib": [f"SDV_{sdv.__version__}"], 
+    #     "Modality": [data_modality], 
+    #     "Synthesizer": [synthesizer_name], 
+    #     "Dataset": [dataset_name], 
+    #     "Train_Time": [train_time - begin_time], 
+    #     "Sample_Time": [sampling_time - train_time], 
+    #     "Peak_Memory_MB": [peak_memory],
+    #     "Synthesizer_Size_MB": [real_dataset_size],
+    #     "Real_Dataset_Size_MB": [],
+    #     "Synthetic_Dataset_Size_MB": [],  
+    #     "Device": ["GPU" if use_gpu else "CPU"]
+    # }
+
+    execution_scores = {
+        # "Date": [today_date],
+        "lib": f"SDV_{sdv.__version__}", 
+        "modality": data_modality, 
+        "synthesizer": synthesizer_name, 
+        "dataset": dataset_name,
+        "num_rows": real_dataset.shape[0], 
+        "num_cols": real_dataset.shape[1], 
+        "num_epochs": num_epochs if num_epochs else 0,
+        # "num_cat": len(dataset_name), 
+        # "num_numeric": len(dataset_name), 
+        "train_time_sec": train_time - begin_time, 
+        "sample_time_sec": sampling_time - train_time, 
+        "peak_memory_mb": peak_memory,
+        "synthesizer_size_mb": synthesizer_size,
+        "real_dataset_size_mb": real_dataset_size,
+        "synthetic_dataset_size_mb": synthetic_dataset_size,  
+        "device": "GPU" if use_gpu else "CPU"
+    }
+
+    # execution_scores = get_execution_scores_obj()
+    # execution_scores["Date"].append(today_date)
+    # execution_scores["Lib"].append(f"SDV_{sdv.__version__}")
+    # execution_scores["Modality"].append(data_modality)
+    # execution_scores["Synthesizer"].append(synthesizer_name)
+    # execution_scores["Dataset"].append(dataset_name)
+    # # TODO:
+    # # add dataset characterstics 
+    # execution_scores["Train_Time"].append(train_time - begin_time)
+    # execution_scores["Peak_Memory_MB"].append(peak_memory)
+    # execution_scores["Synthesizer_Size_MB"].append(synthesizer_size)
+    # execution_scores["Sample_Time"].append(sampling_time - train_time)
+    # execution_scores["Device"].append("GPU" if use_gpu else "CPU")
+
+    # save model
+    synthesizer.save(
+        filepath=f"{output_path}{dataset_name}_{synthesizer_name}_synthesizer.pkl"
+    )
+    # save synthetic data
+    synthetic_data.to_csv(
+        f"{output_path}{dataset_name}_{synthesizer_name}_synthetic_data.csv")
+  
+    # save execution data
+    with open(f"{output_path}{dataset_name}_{synthesizer_name}_execution_scores.json", "w") as json_file:
+        json.dump(execution_scores, json_file)
+
+
+    # execution_scores_df = pd.DataFrame(execution_scores)
+    # execution_scores_df.to_csv(
+    #     f"{output_path}{dataset_name}_{synthesizer_name}_execution_scores.csv")
+
+
+#     quality_report_obj = evaluate_quality(
+#         real_data,
+#         synthetic_data,
+#         metadata
+#     )
+#     print("quality_report_obj: ", quality_report_obj)
+
+#     diagnostic_report_obj = run_diagnostic(
+#         real_data,
+#         synthetic_data,
+#         metadata
+#     )
+#     print("diagnostic_report_obj: ", diagnostic_report_obj)
+            
+# from sdv.datasets.demo import download_demo
+# real_data, metadata = download_demo(
+#     modality='sequential',
+#     dataset_name='nasdaq100_2019'
+# )
+
+
+# df = pd.read_csv("/Users/anshusingh/DPPCC/whitespace/benchmarking-synthetic-data-generators/data/sequential/nasdaq100_2019.csv")
+# groups = df.groupby('Symbol').size().reset_index(name='Count')
+# print(groups, min(list(groups["Count"])),  max(list(groups["Count"])))
+# # for Symbol, Count in zip(list(groups["Symbol"]), list(groups["Count"])):
+# #     print(Symbol, Count)
+# breakpoint()
